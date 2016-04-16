7b3386b120f83ac0acadde7fd49cb4ac9d4dbff7
Joseph Luck <joseph.luck@sky.com>
2016-04-16 14:32:17 +0100

    Styling form input errors

--- scripts/components/FormInput.js
+++ scripts/components/FormInput.js
@@ -5,14 +5,24 @@ function FormInput({
 	error,
 	children
 }) {
+  let form_input_class = "form-input";
+
+  if (error) {
+    form_input_class += ' with-error';
+  }
   return (
-  	<div className="input">
+  	<div className={form_input_class}>
   		{title ?
-  			<label>{title}</label>
+  			<span className="form-input-label">{title}</span>
   			: null
   		}
 	  	{children}
-	  	{error ? <span>{error}</span> : null}
+	  	{error ?
+        <span className="form-input-error">
+          {title || "This value"}{" "}{error}
+        </span>
+        : null
+      }
 	  </div>
   );
 }
