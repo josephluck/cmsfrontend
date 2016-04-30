f0fd8bc038551ff03ee3521b156067c22db72be5
Joseph Luck <joseph.luck@sky.com>
2016-04-30 13:43:45 +0100

    Adding basic implementation of spreadsheet (no functional related things yet!)

--- scripts/components/AttributeForm.js
+++ scripts/components/AttributeForm.js
@@ -13,7 +13,7 @@ table means that edit will work
 without butchering with forced
 uuids in the front-end
 
-*/ ==========================
+========================== */
 
 
 
@@ -22,8 +22,8 @@ uuids in the front-end
 
 import React from 'react';
 import FormHelper from 'utils/FormHelper';
-
 import FormInput from 'components/FormInput';
+import Spreadsheet from 'components/Spreadsheet';
 
 class AttributeForm extends React.Component {
   constructor(props) {
@@ -31,18 +31,14 @@ class AttributeForm extends React.Component {
     this.state = {
       errors: {},
       options: props.attributeCurrentlyEditing.options || [],
-      show_options: false,
-      new_option: {
-        name: "",
-        value: "",
-        errors: {}
-      }
+      show_options: false
     }
 
     if (props.attributeCurrentlyEditing.options && props.attributeCurrentlyEditing.options.length > 0) {
       this.state.show_options = true;
     }
   }
+
   onKindChange(e) {
     if (e.target.value === "dropdown" || e.target.value === "multi_select") {
       this.setState({
@@ -54,118 +50,12 @@ class AttributeForm extends React.Component {
       })
     }
   }
-  handleEditOption(option, e) {
-    e.preventDefault();
-    e.persist();
-    let option_index = this.state.options.findIndex(function(opt, i) {
-      return opt.id === option.id
-    })
 
-    this.state.options[option_index].editing = true;
-    this.forceUpdate();
-
-    setTimeout(() => {
-      let input = e.target.parentElement.parentElement.parentElement.querySelector('input');
-      input.focus();
-    }, 10);
-  }
-  handleEditOptionInputChange(option, attribute, e) {
-    let option_index = this.state.options.findIndex(function(opt, i) {
-      return opt.id === option.id
-    })
-
-    this.state.options[option_index][attribute] = e.target.value;
-    this.forceUpdate();
-  }
-  handleEditSaveOption(option, e) {
-    e.preventDefault();
-    let option_index = this.state.options.findIndex(function(opt, i) {
-      return opt.id === option.id
-    })
-
-    if (option.value.length > 0) {
-      this.state.options[option_index].errors["value"] = undefined
-    } else {
-      this.state.options[option_index].errors["value"] = "Please enter a value";
-    }
-
-    if (option.name.length > 0) {
-      this.state.options[option_index].errors["name"] = undefined
-    } else {
-      this.state.options[option_index].errors["name"] = "Please enter a name";
-    }
-
-    if (option.name.length > 0 && option.value.length > 0) {
-      this.state.options[option_index] = option;
-      this.state.options[option_index].editing = false;
-    }
-
-    this.forceUpdate();
-  }
-  handleDeleteOption(option, e) {
-    e.preventDefault();
-    let option_index = this.state.options.findIndex(function(opt, i) {
-      return opt.id === option.id
-    })
-
-    this.state.options.splice(option_index, 1)
-    this.forceUpdate();
-  }
-  handleNewOptionChange(attribute, e) {
-    this.state.new_option[attribute] = e.target.value;
-    this.forceUpdate();
-  }
-  handleAddNewOption(e) {
-    e.preventDefault();
-
-    if (this.state.new_option.value.length > 0) {
-      this.state.new_option.errors["value"] = undefined;
-    } else {
-      this.state.new_option.errors["value"] = "Please enter a value";
-    }
-
-    if (this.state.new_option.name.length > 0) {
-      this.state.new_option.errors["name"] = undefined;
-    } else {
-      this.state.new_option.errors["name"] = "Please enter a name";
-    }
-
-    if (this.state.new_option.name.length > 0 && this.state.new_option.value.length > 0) {
-      this.state.options.push(this.state.new_option);
-      this.state.new_option = {
-        name: "",
-        value: "",
-        id: Date.now(),
-        errors: {}
-      }
-    }
-
-    this.forceUpdate();
-  }
   onSubmit(e) {
     e.preventDefault();
     let form_values = FormHelper.serialize(e.target);
-
-    this.state.errors = {};
-
-    if (form_values.title === "") {
-      this.state.errors["title"] = "can't be blank";
-      this.forceUpdate();
-      return false
-    } else if ((form_values.kind === "dropdown" || form_values.kind === "multi_select") && this.state.options.length === 0) {
-      this.state.errors["options"] = "Please enter at least one option";
-      this.forceUpdate();
-      return false
-    } else {
-      this.handleAddNewOption(e); // Save the current option if there is one in progress
-      let form = {
-        options: this.state.options,
-        id: this.props.attributeCurrentlyEditing.id || Date.now(),
-        ...form_values
-      }
-      this.props.onSubmit(form, this.props.attributeCurrentlyEditing);
-    }
   }
+
   render() {
     return (
       <form className="modal"
@@ -193,103 +83,28 @@ class AttributeForm extends React.Component {
             </select>
           </FormInput>
 
-          {this.state.show_options === true ?
-            <div>
-              <span className="form-input-label">{"Options"}</span>
-              <ul className="list">
-                <li className="list-header flex">
-                  <span className="flex-1">{"Name"}</span>
-                  <span className="flex-1">{"Value"}</span>
-                  <span className="flex-0 list-buttons">
-                    <span>{"Edit"}</span>
-                    <span>{"Delete"}</span>
-                  </span>
-                </li>
-                {this.state.options.map((option, i) => {
-                  return (
-                    <li className="list-item flex"
-                      key={i}>
-                      {option.editing !== true ?
-                        <span className="flex">
-                          <span className="flex-1">
-                            {option.name}
-                          </span>
-                          <span className="flex-1">
-                            {option.value}
-                          </span>
-                          <span className="flex-0 list-buttons">
-                            <a href=""
-                              onClick={this.handleEditOption.bind(this, option)}>
-                              {"Edit"}
-                            </a>
-                            <a href=""
-                              onClick={this.handleDeleteOption.bind(this, option)}>
-                              {"Delete"}
-                            </a>
-                          </span>
-                        </span>
-                        :
-                        <span className="flex">
-                          <div className="inline-edit-wrapper flex-1">
-                            <FormInput error={option.errors.name}>
-                              <input value={option.name}
-                                onChange={this.handleEditOptionInputChange.bind(this, option, "name")} />
-                            </FormInput>
-                          </div>
-                          <div className="inline-edit-wrapper flex-1">
-                            <div class="form-input">
-                              <FormInput error={option.errors.value}>
-                                <input value={option.value}
-                                  onChange={this.handleEditOptionInputChange.bind(this, option, "value")} />
-                              </FormInput>
-                            </div>
-                          </div>
-                          <span className="flex-0 list-buttons">
-                            <a href=""
-                              onClick={this.handleEditSaveOption.bind(this, option)}>
-                              {"Save"}
-                            </a>
-                            <a href=""
-                              onClick={this.handleDeleteOption.bind(this, option)}>
-                              {"Delete"}
-                            </a>
-                          </span>
-                        </span>
-                      }
-                    </li>
-                  )
-                })}
-                <li className="list-item flex">
-                  <div className="inline-edit-wrapper flex-1">
-                    <FormInput error={this.state.new_option.errors.name}>
-                      <input type="text"
-                        placeholder="Name"
-                        value={this.state.new_option.name}
-                        onChange={this.handleNewOptionChange.bind(this, "name")} />
-                    </FormInput>
-                  </div>
-                  <div className="inline-edit-wrapper flex-1">
-                    <FormInput error={this.state.new_option.errors.value}>
-                      <input type="text"
-                        placeholder="Value"
-                        value={this.state.new_option.value}
-                        onChange={this.handleNewOptionChange.bind(this, "value")} />
-                    </FormInput>
-                  </div>
-                  <span className="flex-0 list-buttons">
-                    <a className="invisible">
-                      {"Delete"}
-                    </a>
-                    <a href=""
-                      onClick={this.handleAddNewOption.bind(this)}>
-                      {"Save"}
-                    </a>
-                  </span>
-                </li>
-              </ul>
-            </div>
-            : null
-          }
+          <Spreadsheet
+            data={this.state.options.concat(this.state.options)}
+            input_models={[
+              {
+                type: 'input',
+                name: 'name',
+                label: 'Name',
+                required: true
+              },
+              {
+                type: 'input',
+                name: 'name',
+                label: 'Name',
+                required: true
+              },
+              {
+                type: 'input',
+                name: 'value',
+                label: 'Value',
+                required: true
+              }
+            ]} />
         </div>
         <div className="modal-footer container text-align-right">
           <a href="" onClick={(e) => {
---
+++ scripts/components/Spreadsheet.js
@@ -0,0 +1,158 @@
+import React from 'react';
+import * as _ from 'underscore';
+import FormHelper from 'utils/FormHelper';
+import FormInput from 'components/FormInput';
+
+class AttributeForm extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			rows: this.generateRows(props.data, props.input_models)
+		}
+	}
+
+	componentWillReceiveProps(props) {
+		console.log(props);
+		this.setState({
+			rows: this.generateRows(props.data, props.input_models)
+		})
+	}
+
+	generateRows(rows, models) {
+		return rows.map((row, i) => {
+	    var spreadsheet_row = {
+	      row_index: i,
+	      column: models.map((model, x) => {
+	        var cell = _.clone(model);
+
+	        cell["column_index"] = x;
+	        cell["value"] = row[cell.name];
+
+	        return cell;
+	      })
+	    };
+	    return spreadsheet_row;
+		});
+	}
+
+	generateEmptyRow(models) {
+	  return {
+	    column: models.map((model, i) => {
+	      var cell = _.clone(model);
+	      cell["column_index"] = i;
+
+	      return cell;
+	    })
+	  };
+	};
+
+	updateSpreadsheetRowIndexes(rows) {
+	  return _.map(rows, function(row, i) {
+	  	row.row_index = i
+	  	return row
+	  });
+	};
+
+	render() {
+		return (
+			<div className="spreadsheet">
+				{this.state.rows.map((row, i) => {
+					return (
+						<div className="flex spreadsheet-row">
+							{row.column.map((cell, x) => {
+								return (
+									<div className="flex-1 spreadsheet-cell">
+										<div className="form-input">
+										  <input name={cell.name}
+										    defaultValue={cell.value}
+										    type="text" />
+										</div>
+									</div>
+								)
+							})}
+							<a className="spreadsheet-delete-link"
+								tabindex="-1">
+								<span className="ss-delete"></span>
+							</a>
+						</div>
+					)
+				})}
+			</div>
+		)
+	}
+}
+
+AttributeForm.defaultProps = {
+	data: [],
+	input_models: []
+}
+
+export default AttributeForm;
+
+
+
+// Spreadsheet.prototype.handleRemoveRow = function(e) {
+//   var row_index;
+//   row_index = parseInt(e.target.dataset.rowIndex);
+//   this.rows.splice(row_index, 1);
+//   this.updateSpreadsheetRowIndexes();
+//   return this.render();
+// };
+
+// Spreadsheet.prototype.handleAddNewRow = function(e) {
+//   this.rows.push(this.generateEmptyRow());
+//   this.updateSpreadsheetRowIndexes();
+//   this.render();
+//   this.last_row = this.el.querySelectorAll('.existing-row')[this.rows.length - 1];
+//   return this.last_row.querySelector('input').focus();
+// };
+
+// Spreadsheet.prototype.updateSpreadsheetRowIndexes = function() {
+//   return _.each(this.rows, (function(_this) {
+//     return function(row, i) {
+//       return row.row_index = i;
+//     };
+//   })(this));
+// };
+
+// Spreadsheet.prototype.generateFormData = function() {
+//   debugger;
+// };
+
+// Spreadsheet.prototype.generateSpreadsheetRowsForTheFirstTime = function() {
+//   var spreadsheet_rows;
+//   spreadsheet_rows = _.map(this.data, (function(_this) {
+//     return function(row, i) {
+//       var spreadsheet_row;
+//       spreadsheet_row = {
+//         row_index: i,
+//         column: _.map(_this.input_models, function(model, x) {
+//           var cell;
+//           cell = _.clone(model);
+//           cell["column_index"] = x;
+//           cell["value"] = row[cell.name];
+//           if (cell.value === "200") {
+//             cell.value = 4;
+//           }
+//           return cell;
+//         })
+//       };
+//       return spreadsheet_row;
+//     };
+//   })(this));
+//   return spreadsheet_rows;
+// };
+
+// Spreadsheet.prototype.generateEmptyRow = function() {
+//   var spreadsheet_row;
+//   spreadsheet_row = {
+//     row_index: this.rows.length,
+//     column: _.map(this.input_models, function(model, i) {
+//       var cell;
+//       cell = _.clone(model);
+//       cell["column_index"] = i;
+//       return cell;
+//     })
+//   };
+//   return spreadsheet_row;
+// };
